5215df42afeaca6af89ea79c7e33751898bdbbba
DESKTOP-O3E55SO\Mateus <msc.mateus.santos@gmail.com>
2024-12-24 18:51:39 -0300

    criados endpoints para geração das imagens e posicionamento dos valores nos campos. Adição de exemplos de posts com as posições

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const sharp = require('sharp');
 const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -32,6 +32,89 @@ app.post('/send-email', (req, res) => {
     }
     res.status(200).json({ message: 'Email sent successfully!', info: info });
   });
+});
+
+const processImage = async (imagePath, outputImagePath, fields, width, height) => {
+  const image = sharp(imagePath);
+
+  const svgText = `
+    <svg width="${width}" height="${height}">
+      ${fields
+        .map(({ text, x, y, fontSize, checkbox }) => {
+          const size = fontSize || 52; 
+
+          if (checkbox) {
+            return `<rect x="${x}" y="${y}" width="18" height="18" fill="black" stroke="black"/>`;
+          }
+          return `<text x="${x}" y="${y}" font-size="${size}" fill="black" font-family="Times New Roman">${text}</text>`;
+        })
+        .join('')}
+    </svg>
+  `;
+
+  const svgBuffer = Buffer.from(svgText);
+
+  await image.composite([{ input: svgBuffer, top: 0, left: 0 }]).toFile(outputImagePath);
+};
+
+app.post('/gen-page-1', async (req, res) => {
+  const { fields } = req.body;
+
+  try {
+    const imagePath = './pdf1.jpg';
+    const outputImagePath = './output1.jpg';
+    await processImage(imagePath, outputImagePath, fields, 2484, 3511);
+
+    res.status(200).json({ message: 'Image updated successfully!', path: outputImagePath });
+  } catch (error) {
+    console.error('Error processing image:', error);
+    res.status(500).json({ error: error.toString() });
+  }
+});
+
+app.post('/gen-page-2', async (req, res) => {
+  const { fields } = req.body;
+
+  try {
+    const imagePath = './pdf2.jpg';
+    const outputImagePath = './output2.jpg';
+    await processImage(imagePath, outputImagePath, fields, 2484, 3511);
+
+    res.status(200).json({ message: 'Image updated successfully!', path: outputImagePath });
+  } catch (error) {
+    console.error('Error processing image:', error);
+    res.status(500).json({ error: error.toString() });
+  }
+});
+
+app.post('/gen-page-3', async (req, res) => {
+  const { fields } = req.body;
+
+  try {
+    const imagePath = './pdf3.jpg';
+    const outputImagePath = './output3.jpg';
+    await processImage(imagePath, outputImagePath, fields, 2484, 3511);
+
+    res.status(200).json({ message: 'Image updated successfully!', path: outputImagePath });
+  } catch (error) {
+    console.error('Error processing image:', error);
+    res.status(500).json({ error: error.toString() });
+  }
+});
+
+app.post('/gen-page-4', async (req, res) => {
+  const { fields } = req.body;
+
+  try {
+    const imagePath = './pdf4.jpg';
+    const outputImagePath = './output4.jpg';
+    await processImage(imagePath, outputImagePath, fields, 2479, 3509);
+
+    res.status(200).json({ message: 'Image updated successfully!', path: outputImagePath });
+  } catch (error) {
+    console.error('Error processing image:', error);
+    res.status(500).json({ error: error.toString() });
+  }
 });
 
 const port = 3000;
